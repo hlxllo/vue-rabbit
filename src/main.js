a96dc473d37bf3b04d3207a441379bcea7b713a7
hlxllo <3337174805@qq.com>
2024-11-21 20:10:12 +0800

    feat:axios

--- src/main.js
+++ src/main.js
@@ -3,4 +3,11 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 
+// 测试接口
+import { getCategoryAPI } from '@/apis/testAPI'
+getCategoryAPI().then(res => {
+  console.log(res)
+})
+
 createApp(App).mount('#app')
+
--- vite.config.js
+++ vite.config.js
@@ -3,16 +3,38 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+// 引入插件
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueDevTools(),
+    // 配置插件
+    AutoImport({
+      resolvers: [ElementPlusResolver()],
+    }),
+    Components({
+      // 配置 elementplus 统一使用 sass 样式
+      resolvers: [ElementPlusResolver({ importStyle: 'sass' })],
+    }),
   ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        // 自动导入定制化样式文件进行样式覆盖
+        additionalData: `
+          @use "@/styles/element/index.scss" as *;
+        `,
+      }
+    }
+  }
 })
